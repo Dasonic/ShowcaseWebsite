13fc9233ab69cef276cbb07a0436d62617bed362
Dasonic <lachlanroberts@protonmail.com>
2020-02-06 12:47:35 +1000

    created projects table in db

---
+++ database/migrations/2020_02_06_013101_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            // $table->timestamps();
+            $table->string('link');
+            $table->string('title', 100);
+            $table->text('description');
+            $table->date('last_updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
